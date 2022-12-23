7ae13903860122f7999998aa24f9768b9ba3c807
Jeremiah Kendl <kendljeremiah@gmail.com>
2022-12-23 17:17:23 -0600

    same as prev

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,39 +16,35 @@ function About() {
       </ul>
       <ul class="Now">
         <ul>
-          <span>
-          👋 Hi, I’m Jeremiah
-          </span>
-        </ul>
-        <ul>
-        <span>
-          👀 I’m interested in changing careers from Legal Processes and
-          Administration to Software Development and Programming
-        </span>
-        </ul>
-        <ul>
-        <span>
-          🥾 I've graduated from the University of Minnesota- Twin Cities Full
-          Stack Bootcamp and am currently refining my skills and making more
-        </span>
+          <span>👋 Hi, I’m Jeremiah</span>
         </ul>
         <ul>
           <span>
-            🌱 I’m currently learning Python 3.0 from FreeCodeCamp
+            👀 I’m interested in changing careers from Legal Processes and
+            Administration to Software Development and Programming
           </span>
         </ul>
         <ul>
-        <span>
-          💞️ I’m looking to collaborate on open source projects. What really
-          interests me are social media APIs, Machine Learning, Open Source
-          Projects, and Unconventional Website Design.
-        </span>
+          <span>
+            🥾 I've graduated from the University of Minnesota- Twin Cities Full
+            Stack Bootcamp and am currently refining my skills and making more
+          </span>
         </ul>
         <ul>
-        <span>
-          📫 You can reach me @juicyjerrry on twitter or email at
-          kendljeremiah@gmail.com
-        </span>
+          <span>🌱 I’m currently learning Python 3.0 from FreeCodeCamp</span>
+        </ul>
+        <ul>
+          <span>
+            💞️ I’m looking to collaborate on open source projects. What really
+            interests me are social media APIs, Machine Learning, Open Source
+            Projects, and Unconventional Website Design.
+          </span>
+        </ul>
+        <ul>
+          <span>
+            📫 You can reach me @juicyjerrry on twitter or email at
+            kendljeremiah@gmail.com
+          </span>
         </ul>
       </ul>
     </header>
