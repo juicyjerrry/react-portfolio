ccc14114413eb723f8569ae6d01e670dfbdec0c7
Jeremiah Kendl <kendljeremiah@gmail.com>
2022-12-23 17:17:04 -0600

    Working on scroll into view
    Rn hello is transform L to R
    TODO get it to run as soon as page is loaded

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,33 +7,49 @@ import "../../styles/About.css";
 
 function About() {
   return (
-    <header className="header">
+    <header class="header">
       <ul>
-        <img src={hello} alt="hello gif" />
+        <img class="car" src={hello} alt="hello gif" />
       </ul>
-      <ul className="profilePic">
+      <ul class="profilePic">
         <img src={Profile} alt="Recent picture of me" />
       </ul>
-      <ul>👋 Hi, I’m Jeremiah</ul>
-      <ul>
-        👀 I’m interested in changing careers from Legal Processes and
-        Administration to Software Development and Programming
-      </ul>
-      <ul>
-        🥾 I've graduated from the University of Minnesota- Twin Cities Full
-        Stack Bootcamp and am currently refining my skills and making more
-      </ul>
-      <ul>
-        🌱 I’m currently learning Python 3.0 from FreeCodeCamp
-      </ul>
-      <ul>
-        💞️ I’m looking to collaborate on open source projects. What really
-        interests me are social media APIs, Machine Learning, Open Source
-        Projects, and Unconventional Website Design.
-      </ul>
-      <ul>
-        📫 How to reach me @juicyjerrry on twitter or my email is
-        kendljeremiah@gmail.com
+      <ul class="Now">
+        <ul>
+          <span>
+          👋 Hi, I’m Jeremiah
+          </span>
+        </ul>
+        <ul>
+        <span>
+          👀 I’m interested in changing careers from Legal Processes and
+          Administration to Software Development and Programming
+        </span>
+        </ul>
+        <ul>
+        <span>
+          🥾 I've graduated from the University of Minnesota- Twin Cities Full
+          Stack Bootcamp and am currently refining my skills and making more
+        </span>
+        </ul>
+        <ul>
+          <span>
+            🌱 I’m currently learning Python 3.0 from FreeCodeCamp
+          </span>
+        </ul>
+        <ul>
+        <span>
+          💞️ I’m looking to collaborate on open source projects. What really
+          interests me are social media APIs, Machine Learning, Open Source
+          Projects, and Unconventional Website Design.
+        </span>
+        </ul>
+        <ul>
+        <span>
+          📫 You can reach me @juicyjerrry on twitter or email at
+          kendljeremiah@gmail.com
+        </span>
+        </ul>
       </ul>
     </header>
   );
